f9480d4e3527d0627005a1ac46c80c1fe7372d67
Dragomir Yordanov <bad.stupid.monkey@gmail.com>
2020-03-23 19:33:47 +0100

    Use single file to store Cloudflare IPS and use absolute routes instead of switching directory

--- ufw-cf.sh
+++ ufw-cf.sh
@@ -1,28 +1,20 @@
 #!/bin/sh
 
-cd /tmp
-wget https://www.cloudflare.com/ips-v4 -O cfips-v4.tmp
-wget https://www.cloudflare.com/ips-v6 -O cfips-v6.tmp
+touch /tmp/cloudflare-ips.tmp
+wget https://www.cloudflare.com/ips-v4 -a /tmp/cloudflare-ips.tmp
+wget https://www.cloudflare.com/ips-v6 -a /tmp/cloudflare-ips.tmp
 
-touch ufw-remove-cloudflare.sh
-rm ufw-remove-cloudflare.sh
+touch /tmp/ufw-remove-cloudflare.sh
+rm /tmp/ufw-remove-cloudflare.sh
 
-for cfip in `cat cfips-v4.tmp`; do
+for cfip in `cat cloudflare-ips.tmp`; do
         ufw allow from $cfip to any port 80 proto tcp comment "cloudflare"
         ufw allow from $cfip to any port 443 proto tcp comment "cloudflare"
         echo "ufw delete allow from $cfip to any port 80 proto tcp" >> ufw-remove-cloudflare.sh
         echo "ufw delete allow from $cfip to any port 443 proto tcp" >> ufw-remove-cloudflare.sh
 done
 
-for cfip in `cat cfips-v6.tmp`; do
-        ufw allow from $cfip to any port 80 proto tcp comment "cloudflare"
-        ufw allow from $cfip to any port 443 proto tcp comment "cloudflare"
-        echo "ufw delete allow from $cfip to any port 80 proto tcp" >> ufw-remove-cloudflare.sh
-        echo "ufw delete allow from $cfip to any port 443 proto tcp" >> ufw-remove-cloudflare.sh
-done
-
-rm cfips-v4.tmp
-rm cfips-v6.tmp
+rm /tmp/cloudflare-ips.tmp
 
 echo 'Done.'
 echo 'A .sh file has been generated in /tmp/ufw-remove-cloudflare.sh to remove the rules that were added.'
